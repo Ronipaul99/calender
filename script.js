6ecef50b9972fbe0e844a64735e9d3c08cd3f38a
Roni paul <ronipaul9972@gmail.com>
2024-06-22 17:19:07 +0530

    added other months date

--- script.js
+++ script.js
@@ -36,16 +36,52 @@ const setCurrentMonth = () => {
     monthName.innerText=`${currentMonth} ${currentYear}`
 }
 
+Array.prototype.count = function (item){
+    let result = {
+        index : [],
+        count : 0
+    }
+    result.count = this.filter(
+        (data , index)=>{
+            if(data===item){
+                result.index.push(index)
+                return data
+            }
+        }
+    ).length
+    return result
+}
 const setDatesInMonth = (year = new Date().getFullYear() , month =  new Date().getMonth() ) => {
     calenderBody.innerHTML=''
     const days = daysInMonth(year , month)
     let daysArray = [...Array((new Date(`${month+1}-01-${year}`).getDay())).keys()].map(i=>days-i).reverse()
     daysArray.push(... [...Array(days).keys()].map(i=>i+1))
+    if((daysArray.length%7)!==0){
+        daysArray.push(... [...Array(7-(daysArray.length%7)).keys()].map(i=>i+1))
+    }
+
     daysArray=daysArray.reverse()
-    daysArray.forEach(day=>{
+    daysArray.forEach((day,index)=>{
+        let monthVal = month
+        let countResult = daysArray.count(day)
+        if (day>15 && countResult.count>1 && countResult.index[0] === index) {
+            monthVal = month+1
+        }
+        if (day<15 && countResult.count>1 && countResult.index[1] === index) {
+            monthVal = month-1
+        }
         document.querySelector('#calenderDays').insertAdjacentHTML(
             'afterbegin',
-            `<div class="calender-day"><div class="day-number" id="${day+'-'+month+'-'+year}">${day}</div></div>` 
+            `<div class="calender-day">
+                <div 
+                    class="day-number 
+                            ${day>15 && countResult.count>1 && countResult.index[1] === index ? 'day-other-month':''}
+                            ${day<15 && countResult.count>1 && countResult.index[0] === index ? 'day-other-month':''}"
+                    id="${day+'-'+monthVal+'-'+year}"
+                >
+                    ${day}
+                </div>
+            </div>` 
         )
     })
     setCurrentDate()
